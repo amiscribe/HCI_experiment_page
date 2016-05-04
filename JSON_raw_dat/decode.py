f541a3435da675f52f7478b4b23c3bf7e930dfd4
Connor Scully <amilex@gmail.com>
2016-05-04 09:43:46 -0700

    data bagged and tagged

--- JSON_raw_dat/decode.py
+++ JSON_raw_dat/decode.py
@@ -19,6 +19,7 @@ def storeTrialTimes( allTrials ):
     #variables
     allTrialsStr = ""
     allTrialsFile = allTrials['subject'] + "_all_trials" + ".csv"
+    inChars = ",,"
     csvOut = ""
     fileout = ""
     acc = 0
@@ -28,50 +29,82 @@ def storeTrialTimes( allTrials ):
     ###for output & outputs
     for y in range(0, len(allTrials['data'])):
         fileout = (allTrials['subject'] + "-" + ("trial" + str(y)) + ".csv")
-        csvOut = "Trial" + str(y) + ","
+       
+        #append the two strings showing intended
+        #input and given input 
+        csvOut = "\"" + allTrials['data'][y]['errors']['cmpStr'] + "\"" + ","
+        csvOut += "\"" + allTrials['data'][y]['errors']['input'] + "\"" + ","
 
         inDat = allTrials['data'][y]['inputData']
 
         for x in range(1, len(inDat)):
+            inChars += inDat[x].get('letter', '\0').encode('utf-8') + ","
             csvOut = csvOut + (str(inDat[x]['time']) + ",")
             acc += inDat[x]['time']
     
         #compute average for given trial and append
         avg = acc/len(inDat)
-        csvOut += str(avg) + "\n"
+        inChars += "\n"
+        csvOut += "," + str(avg) + "\n"
 
         #append to all trials
-        allTrialsStr += csvOut        
-
+        allTrialsStr += csvOut.encode('utf-8')        
+    
+        csvOut = inChars + csvOut.encode('utf-8')
         outputCSV(fileout, csvOut)
 
         csvOut = ""
-    
+        inChars = ",,"
+        acc = 0
+
     ##Output all trials
     outputCSV(allTrialsFile, allTrialsStr)
 
 
 
+def getErrors(errorData):
+    #variables
+    activeErr = 0
+    
+    ##Evaluate errors as # of keystrokes greater than
+    ##min amout to complete task
+    activeErr += (len(errorData['input']) - len(errorData['cmpStr']))
+
+    return activeErr
+
 ##############################################
 #Main Driver##################################
 ##############################################
 
-###Variables################
+
+###Variables##############
 
 
 filename = raw_input('Please enter the filename to be decoded:')
 
-#read in the json string
+##read in the json string
 JSON_file = open(filename, 'r')
 JSON_string = JSON_file.read()
 JSON_file.close()
 
-#convert to python data structures
+##convert JSON to python data structures
 anima_data = json.loads(JSON_string)
-
 subject = anima_data['subject']
+
 
 storeTrialTimes(anima_data)
 
-#print trial1
+
+
+
+
+
+
+
+
+
+
+
+
+
 
